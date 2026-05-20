89c1cdbd487f4ddcbd416e655636fe7ff483e14f
AlexSamano67 <ale.samano05@gmail.com>
2026-05-20 09:51:33 -0600

    logs the score and choices as well as the result for the round but when keeping score sometimes tallies incorrect

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
 console.log("Hello World");
-        let humanScore = 0;
-        let computerScore = 0;
-function scoreGame() {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        const result = playRound(humanChoice, computerChoice);
-        if (result === "You win!") {
-            (humanScore++);
-        } else if (result === "You lose!") {
-            (computerScore++);
-        }
-    }
-
-function getLeader() {
-    if (humanScore === computerScore) {
-        return (" Tie!")
-    } else if (computerScore > humanScore) {
-        return ("Computer leads!")
-    } else {
-        return ("Human leads!")
-    }
-}
-    for (let i = 0; i < 5; i++) {
+let humanScore = 0;
+let computerScore = 0;
+for (let i = 0; i < 10; i++) {
     const humanChoice = getHumanChoice();
     const computerChoice = getComputerChoice();
-        console.log("You chose: " + humanChoice);
-        console.log("Computer chose: " + computerChoice);
-        console.log(playRound(humanChoice, computerChoice));
-        console.log("Score: Human " + humanScore + " - Computer " + computerScore);
-        console.log(getLeader());
-    }
+    const result = playRound(humanChoice, computerChoice);
+    
+    console.log("You chose: " + humanChoice);
+    console.log("Computer chose: " + computerChoice);
+    console.log(result);
 
+    if (result === "You win!") humanScore++;
+    if (result === "You lose!") computerScore++;
+    console.log("Current score - You: " + humanScore + " Computer: " + computerScore);
+    if (result === "You win!") humanScore++;
+}
+   
 
 function getComputerChoice() {
     const choice = ["rock", "paper", "scissors"];
@@ -38,6 +23,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+    if (typeof prompt === "function") {
     const choice = prompt("Enter rock, paper, or scissors:");
     if (choice === "rock" || choice === "paper" || choice === "scissors") {
         return choice;
@@ -45,33 +31,46 @@ function getHumanChoice() {
         alert("Invalid choice. Please try again.");
         return getHumanChoice();
     }
+} 
 }
 function playRound( humanChoice, computerChoice) {
     if (computerChoice === "rock") {
     if (humanChoice === "rock") {
-        return "It's a tie!";
+        return result = "It's a tie!";
     } else if (humanChoice === "paper") {
-        return "You win!";
-    } else if (humanChoice === "scissors") {    
-        return "You lose!";
+        return result = "You win!";
+    } else if (humanChoice === "scissors") {
+        return result = "You lose!";
     }
 }
     if (computerChoice === "paper") {
     if (humanChoice === "rock") {
-        return "You lose!";
+        return result = "You lose!";
     } else if (humanChoice === "paper") {
-        return "It's a tie!";
+        return result = "It's a tie!";
     } else if (humanChoice === "scissors") {
-        return "You win!";
+        return result = "You win!";
     }
 }
     if (computerChoice === "scissors") {
         if (humanChoice === "rock") {
-            return "You win!";
+            return result = "You win!";
         } else if (humanChoice === "paper") {
-            return "You lose!";
+            return result = "You lose!";
         } else if (humanChoice === "scissors") {
-            return "It's a tie!";
+            return result = "It's a tie!";
         }
+    }
+    }
+
+
+
+function getLeader() {
+    if (humanScore % computerScore === 0) {
+        return (" Score is tied!")
+    } else if (computerScore % humanScore > 0) {
+        return ("Computer leads!")
+    } else {
+        return ("Human leads!")
     }
 }
